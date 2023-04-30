91a033d42f0f1656376d7a212fd748e26be6361b
dm-mrtnvch <dmvshn@gmail.com>
2023-04-30 19:01:36 +0400

    feat: css modules / theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import {Suspense} from "react";
 import {Link, Route, Routes} from "react-router-dom";
-import './index.scss'
+import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+
+ const {theme, toggleTheme} = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`} id="main-theme">
+      <button onClick={toggleTheme}>change theme</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>ABOUT</Link>
       <Suspense fallback={<div>Loading...</div>}>
