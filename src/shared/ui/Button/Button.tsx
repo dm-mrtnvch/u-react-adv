c26ec0ceb449dfb829db1222e9d2a2fa7982ce2b
dm-mrtnvch <dmvshn@gmail.com>
2023-05-03 23:35:54 +0400

    feat: ui-kit - Button

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,29 @@
+import {ButtonHTMLAttributes, FC} from "react";
+import classNames from "shared/lib/classNames/classNames";
+import cls from "./Button.module.scss"
+
+export enum ThemeButton {
+  CLEAR = 'clear',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string
+  theme?: ThemeButton
+}
+
+const Button: FC<ButtonProps> = (props) => {
+  const {
+    className,
+    children,
+    theme,
+    ...otherProps
+
+  } = props
+  return (
+    <button className={classNames(cls.Button, {[cls[theme]]: true}, [className])} {...otherProps}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,5 +1,8 @@
-import {useTheme} from "app/providers/ThemeProvider";
+import {Theme, useTheme} from "app/providers/ThemeProvider";
+import DarkIcon from 'shared/assets/icons/theme-dark.svg'
+import LightIcon from 'shared/assets/icons/theme-light.svg'
 import classNames from "shared/lib/classNames/classNames";
+import Button, {ThemeButton} from "shared/ui/Button/Button";
 import cls from "./ThemeSwitcher.module.scss"
 
 interface ThemeSwitcherProps {
@@ -10,10 +13,13 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
   const {theme, toggleTheme} = useTheme()
 
   return (
-    <button className={classNames(cls.ThemeSwitcher, {}, [className])}
-      onClick={toggleTheme}>
-      change theme
-    </button>
+    <Button
+      theme={ThemeButton.CLEAR}
+      className={classNames(cls.ThemeSwitcher, {}, [className])}
+      onClick={toggleTheme}
+    >
+      {theme === Theme.DARK ? <DarkIcon/> : <LightIcon/>}
+    </Button>
   );
 };
 
