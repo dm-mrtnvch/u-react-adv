c5a91f1e4f2fec630a520fd002c89035173166d6
dm-mrtnvch <dmvshn@gmail.com>
2023-05-09 01:56:45 +0400

    config: jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,39 @@
+import classNames from './classNames';
+
+describe('classNames', () => {
+  test('with only one param', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('with additional class', () => {
+    const expected = 'someClass class1 class2';
+    expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with additional class and truthy mods', () => {
+    const expected = 'someClass class1 class2 hover scrollable';
+    expect(classNames(
+      'someClass',
+      { hover: true, scrollable: true },
+      ['class1', 'class2'],
+    )).toBe(expected);
+  });
+
+  test('with additional class and truthy/falsy mods', () => {
+    const expected = 'someClass class1 class2 hover';
+    expect(classNames(
+      'someClass',
+      { hover: true, scrollable: false },
+      ['class1', 'class2'],
+    )).toBe(expected);
+  });
+
+  test('with additional class and null/undefined mods', () => {
+    const expected = 'someClass class1 class2';
+    expect(classNames(
+      'someClass',
+      { hover: null, scrollable: undefined },
+      ['class1', 'class2'],
+    )).toBe(expected);
+  });
+});
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,7 +4,7 @@ function classNames(cls: string, mods: Mods = {}, additional: string[] = []): st
   return [
     cls,
     ...additional.filter(Boolean),
-    Object.entries(mods)
+    ...Object.entries(mods)
       .filter(([_, value]) => Boolean(value))
       .map(([className]) => className),
   ]
