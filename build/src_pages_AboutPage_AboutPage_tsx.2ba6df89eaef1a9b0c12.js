3992834d6724617eea8b3526a05bebe3f648f066
dm-mrtnvch <dmvshn@gmail.com>
2023-05-14 19:01:01 +0400

    Revert "fix: pages spread operator"
    This reverts commit cdd8a4f1043efcb70ac37235318bda221b19c56b.

---
+++ build/src_pages_AboutPage_AboutPage_tsx.2ba6df89eaef1a9b0c12.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunkreact_advanced"] = self["webpackChunkreact_advanced"] || []).push([["src_pages_AboutPage_AboutPage_tsx"],{
+
+/***/ "./src/pages/AboutPage/AboutPage.tsx":
+/*!*******************************************!*\
+  !*** ./src/pages/AboutPage/AboutPage.tsx ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+var AboutPage = function () {
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "AboutPage" }, void 0));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AboutPage);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX0Fib3V0UGFnZV9BYm91dFBhZ2VfdHN4LjJiYTZkZjg5ZWFlZjFhOWIwYzEyLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQWdEO0FBQ2hEO0FBQ0EsWUFBWSxzREFBSSxVQUFVLHVCQUF1QjtBQUNqRDtBQUNBLGlFQUFlLFNBQVMsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3JlYWN0LWFkdmFuY2VkLy4vc3JjL3BhZ2VzL0Fib3V0UGFnZS9BYm91dFBhZ2UudHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGpzeCBhcyBfanN4IH0gZnJvbSBcInJlYWN0L2pzeC1ydW50aW1lXCI7XG52YXIgQWJvdXRQYWdlID0gZnVuY3Rpb24gKCkge1xuICAgIHJldHVybiAoX2pzeChcImRpdlwiLCB7IGNoaWxkcmVuOiBcIkFib3V0UGFnZVwiIH0sIHZvaWQgMCkpO1xufTtcbmV4cG9ydCBkZWZhdWx0IEFib3V0UGFnZTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -12,7 +12,7 @@ export default {
     },
 } as ComponentMeta<typeof MainPage>;
 
-const Template: ComponentStory<typeof MainPage> = (args) => <MainPage { ...(args as object) } />;
+const Template: ComponentStory<typeof MainPage> = (args) => <MainPage {...args} />;
 
 export const Normal = Template.bind({});
 Normal.args = {};
