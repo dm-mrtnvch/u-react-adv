cdd8a4f1043efcb70ac37235318bda221b19c56b
dm-mrtnvch <dmvshn@gmail.com>
2023-05-14 17:03:39 +0400

    fix: pages spread operator

--- src/pages/AboutPage/ui/AboutPage.stories.tsx
+++ src/pages/AboutPage/ui/AboutPage.stories.tsx
@@ -12,7 +12,7 @@ export default {
     },
 } as ComponentMeta<typeof AboutPage>;
 
-const Template: ComponentStory<typeof AboutPage> = (args) => <AboutPage {...args} />;
+const Template: ComponentStory<typeof AboutPage> = (args) => <AboutPage { ...(args as object) } />;
 
 export const Normal = Template.bind({});
 Normal.args = {};
