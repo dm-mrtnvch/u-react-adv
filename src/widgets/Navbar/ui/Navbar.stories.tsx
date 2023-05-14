291be3594b9f65d1c3a2b0636a6cbb3848bcbd84
dm-mrtnvch <dmvshn@gmail.com>
2023-05-14 18:58:37 +0400

    Revert "fix: pages spread operator"
    This reverts commit cdd8a4f1043efcb70ac37235318bda221b19c56b.

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -13,7 +13,7 @@ export default {
   },
 } as ComponentMeta<typeof Navbar>;
 
-const Template: ComponentStory<typeof Navbar> = (args) => <Navbar { ...(args as object) } />;
+const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 
 export const Light = Template.bind({});
 Light.args = {};
