05d9da1eb31b042b7a24f8e6c4543cad4db32d7b
dm-mrtnvch <dmvshn@gmail.com>
2023-05-14 17:03:39 +0400

    fix: pages spread operator

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -13,7 +13,7 @@ export default {
   },
 } as ComponentMeta<typeof Navbar>;
 
-const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
+const Template: ComponentStory<typeof Navbar> = (args) => <Navbar { ...(args as object) } />;
 
 export const Light = Template.bind({});
 Light.args = {};
