83b06799aa92d03c5178155c6436d3374b396517
dm-mrtnvch <dmvshn@gmail.com>
2023-07-04 02:32:29 +0400

    feat: shared/ui/Card

--- src/entities/Article/ui/ArticlesListItem/ArticlesListItem.tsx
+++ src/entities/Article/ui/ArticlesListItem/ArticlesListItem.tsx
@@ -1,6 +1,10 @@
 import { memo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { classNames } from 'shared/lib/classNames/classNames'
+import { Card } from 'shared/ui/Card/Card'
+import { Text } from 'shared/ui/Text/Text'
+import { Icon } from 'shared/ui/Icon/Icon'
+import EyeIcon from 'shared/assets/icons/eye-20-20.svg'
 import { Article, ArticleView } from '../../model/types/article'
 import cls from './ArticlesListItem.module.scss'
 
@@ -14,9 +18,28 @@ export const ArticlesListItem = memo((props: ArticlesListItemProps) => {
   const { className, article, view } = props
   const { t } = useTranslation()
 
+  if (view === ArticleView.BIG) {
+    return (
+      <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
+        {article.title}
+      </div>
+    )
+  }
+
   return (
-    <div className={classNames(cls.ArticleListItem, {}, [className])}>
-      {article.title}
+    <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
+      <Card className={cls.card} >
+        <div className={cls.imageWrapper}>
+          <img src={article.img} className={cls.img} alt={article.title} />
+          <Text text={article.createdAt} className={cls.date} />
+        </div>
+        <div className={cls.infoWrapper}>
+          <Text text={article.type.join(', ')} className={cls.types} />
+          <Text text={String(article.views)} className={cls.views} />
+          <Icon Svg={EyeIcon} />
+        </div>
+        <Text text={article.title} className={cls.title} />
+      </Card>
     </div>
   )
 })
