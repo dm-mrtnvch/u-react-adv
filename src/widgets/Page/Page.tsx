b1a16b4cc3239124d8d2d719809368224ca88f61
dm-mrtnvch <dmvshn@gmail.com>
2023-07-13 23:26:40 +0400

    fix: fetch data on scroll

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -52,7 +52,11 @@ export const Page = memo((props: PageProps) => {
       onScroll={onScroll}
     >
       {children}
-      <div ref={triggerRef} />
+      {
+        onScrollEnd
+          ? <div className={cls.trigger} ref={triggerRef} />
+          : null
+      }
     </section>
   )
 })
