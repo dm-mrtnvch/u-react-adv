f5c423d8363b348214f744ace247511c48660400
dm-mrtnvch <dmvshn@gmail.com>
2023-08-05 21:29:21 +0400

    feat: translate locales

---
+++ src/locales/399.790e8fe1ade0dd3953f8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreact_advanced=self.webpackChunkreact_advanced||[]).push([[399],{8399:(a,e,n)=>{n.r(e),n.d(e,{default:()=>d});var c=n(5893),t=n(4478),r=n(314);const d=function(){var a=(0,t.$)("about").t;return(0,c.jsx)(r.T,{children:a("admin panel")})}}}]);
