5aea30d84d7fbc348f59f41b38aed3006940ce7e
dm-mrtnvch <dmvshn@gmail.com>
2023-08-10 19:51:28 +0400

    config: browserlist | improve: Drawer Animation Provider

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -3,7 +3,7 @@ import React, {
   memo, ReactNode, useCallback, useEffect,
 } from 'react'
 import { useTheme } from 'app/providers/ThemeProvider'
-import { useAnimationLibs } from '../../lib/components/AnimationProvider'
+import { AnimationProvider, useAnimationLibs } from '../../lib/components/AnimationProvider'
 import { Overlay } from '../Overlay/Overlay'
 import cls from './Drawer.module.scss'
 import { Portal } from '../Portal/Portal'
@@ -96,7 +96,7 @@ export const DrawerContent = memo((props: DrawerProps) => {
   )
 })
 
-export const Drawer = memo((props: DrawerProps) => {
+const DrawerAsync = (props: DrawerProps) => {
   const { isLoaded } = useAnimationLibs()
 
   if (!isLoaded) {
@@ -104,4 +104,12 @@ export const Drawer = memo((props: DrawerProps) => {
   }
 
   return <DrawerContent {...props} />
-})
+}
+
+export const Drawer = (props: DrawerProps) => {
+  return (
+    <AnimationProvider>
+      <DrawerAsync {...props} />
+    </AnimationProvider>
+  )
+}
